b6ad1026bbba22f9c3ad12a02f9fdb6b875c61bd
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2024-08-31 06:44:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,3 @@
-import os
-from flask import Flask
-import logging
-from telegram import Bot
-
-app = Flask(__name__)
-
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-
-# Create the bot instance
-TOKEN = os.getenv('TELEGRAM_TOKEN')  # Your Telegram bot token
-bot = Bot(token=TOKEN)
-
-@app.route('/')
-def home():
-    return "Telegram bot is running!"
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", "10000"))  # Use port from environment variable or default to 10000
+    port = int(os.environ.get("PORT", "10000"))
     app.run(host='0.0.0.0', port=port)
